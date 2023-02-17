16572b6c94f673f387b9df8a41375f0e05d04e07
komalramdaskhalkar <komalramdaskhalkar@gmail.com>
2023-02-17 10:28:23 +0530

    assignment1 variable.js
    assignment1 variable.js

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,14 @@
 console.log("komal khalkar");
+console.log("26");
+console.log("Narayngavhan");
+
+var FullName="komal khalkar";
+console.log(FullName);
+console.log(FullName);
+
+var city;
+console.log(city);
+city="Narayangavhan";
+console.log(city);
+city="Narayangavhan";
+
---
+++ variable.js
@@ -0,0 +1,32 @@
+console.log("************ step 1 ***********");
+ var MyFullName = "Komal Khalkar";
+console.log("MyFullName: Komal Khalkar");
+
+var MyFullName="Komal Ramdas khalkar";
+console.log("MyFullName: Komal Ramdas khalkar");
+
+console.log("************ step 2 ************");
+var mylove ="I loves only JS";
+console.log( "mylove:" ,mylove);
+
+console.log("************ step 3 *************");
+var firstName="komal";
+console.log("firstName:", firstName);
+
+var lastName="khalkar";
+console.log("lastName :" , lastName);
+
+var age="26";
+console.log("age:" , age);
+
+ var address="Narayngavhan";
+ console.log("adress:", address);
+
+ var pincode="414301";
+ console.log("pincode:", pincode);
+
+ var gender="female";
+ console.log("gender:", gender);
+
+ var ismarried="yes";
+ console.log("ismarried:", ismarried);
