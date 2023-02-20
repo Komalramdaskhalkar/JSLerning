b3298b1f74e0af9e13334ed68bad48cc2aedae3b
komalramdaskhalkar <komalramdaskhalkar@gmail.com>
2023-02-20 15:21:52 +0530

    033_funassign20feb.js completed
    033_funassign20feb.js completed

---
+++ 033_funassign20feb.js
@@ -0,0 +1,40 @@
+console.log("*****step 1 ******");
+console.log(" Function1 with no argument and no return value");
+console.log(" Function2 with no argument and no return value");
+console.log("**************step 2***********");
+
+var personalDetails =" komalkhalkar,AnnasahebAawtecollagemanchar";
+console.log("komal Khalkar,Annasaheb Aawte collage manchar");
+ 
+
+
+console.log("**************step 3***********");
+var name1= "virat";
+var name2="anushka";
+ var num1 = 1000;
+ var num2 = 2000;
+ 
+ function swapValueDude( value1, value2 ) {
+    
+   console.log( "Before swap:" ,value3, value4 );
+    var name2 = value3;
+    value3= value4;
+      value4= name2;
+   console.log("After swap:" ,value3, value4 );
+} swapValueDude( name1 ,name2);
+ 
+ function swapValueDude( value1, value2 ) {
+    
+    console.log( "Before swap:" ,value1, value2 );
+     var num1 = value1;
+     value1= value2;
+       value2= num1;
+    console.log("After swap:" ,value1, value2 );
+   
+ } swapValueDude( num1 ,num2 );
+
+ console.log("*********** step 4 **********" )
+ var value= "10.23,40,600";
+  console.log(" 10.23,600,40");
+  var value ="Hello, Good, Morning";
+console.log("Hello Good Morning ");
