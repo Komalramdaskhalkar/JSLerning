08b2c10027134a70af8a48b2b23656d480e10f2f
komalramdaskhalkar <komalramdaskhalkar@gmail.com>
2023-02-27 19:32:47 +0530

    operatorAssign.js completed
    operatorAssign.js completed

--- 05_operator.js
+++ 05_operator.js
@@ -18,5 +18,47 @@ var num2=100;
  
   result = num1 /3;
   console.log(`remainder is: ${result}`);
-  
+   
 
+
+var num=10;
+num+=20;
+console.log(`compound addition +=${num}`);
+
+console.log(`caparision operators `)
+ var num3="10";
+ var num4= 10;
+
+ console.log(num3==10);
+ console.log(num3==11);
+ console.log(num3===num4);
+
+ console.log(num3 != num4);
+  
+ console.log(10>20);
+ console.log(10<20);
+ var marks=35;
+ console.log( marks>=35);
+
+
+
+  console.log(10 < 10);
+   var marks =70;
+   var result=marks >=60 ? "Allow  Him for interview" :" Don't allow ";
+   console.log(result);
+
+   var age=22;
+   var res =age>=21? "congratulation..." :  "Ha haa  No next time";
+   console.log(res);
+
+   console.log("EVEN or ODD ");
+   
+   var myName=7;
+   console.log(myName%2 == 0);
+    var result = myName%2 == 0? "EVEN": "ODD";
+console.log(result);
+
+var num1=10;
+var num2=12;
+  var res= num1>num2 ? num1:num2;
+  console.log(res); 
---
+++ 0Aassign.js
@@ -0,0 +1,31 @@
+
+console.log("-----------step 2----------");
+var num1="29";
+var num2 ="44";
+var num3="0";
+var num4="101";
+ function isEvenOrOdd(num1,num2,num3,num4){
+    var result= (num1%2==0 )
+    console.log( `The given number 29 is EVEN(TRUE)or ODD(FALSE):${result} `);
+    var result= (num2%2==0 )
+    console.log( `The given number 44 is EVEN(TRUE)or ODD(FALSE) :${result}`);
+    var result= (num3%2==0 );;
+    console.log( `The given number 0 is EVEN(TRUE)or ODD(FALSE):${result}`);
+    var result= (num4%2==0 );
+    console.log( `The given number is EVEN(TRUE)or ODD(FALSE): ${result}`);
+ }isEvenOrOdd(num1,num2,num3,num4);
+  var value1= "Javascript";
+  var value2=  "developer";
+  var value3=  "Google"; 
+  console.log("-----------------------------------------------------")
+  function WordLength(value1){ 
+   var result= value1.length%2==0 ? "EVEN":"ODD";
+   
+   console.log( `The given wordlength is ${value1}: ${result}`);
+   
+   } 
+   WordLength(value1);
+    WordLength(value2);
+   WordLength(value3);
+
+ 
