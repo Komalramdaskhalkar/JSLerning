67bf76b9a15f544f0f023d43dd5edf48e096295c
komalramdaskhalkar <komalramdaskhalkar@gmail.com>
2023-03-03 20:35:34 +0530

    OB_operatorassign.js completed
    OB_operatorassign.js completed

--- 0B_operatorassign.js
+++ 0B_operatorassign.js
@@ -1,1 +1,17 @@
-console.log("+++++++++++++++ step1 ++++++++++++++++++++")
+console.log("+++++++++++++++ step1 ++++++++++++++++++++");
+function maleMarriageEligibillity( gender,age,boyName ){
+    var result= ( gender,age>=21,boyName ) //true|| false||false-->
+    ? `congrates hey ${boyName} you are eligible for marriage`:
+    `elese not eligible for marriage`;
+    console.log(result);
+}femaleMarriageEligibillity("male",25,"Billgates");
+maleMarriageEligibillity("male",17,"Stew Jobs");
+
+console.log("+++++++++++++++ step1 ++++++++++++++++++++");
+function femaleMarriageEligibillity( gender,age,girlName ){
+    var result=(gender,age>=18,girlName) //true|| false||false-->
+    ? `congrates ${girlName} you are eligible for marriage`:
+    `elese not eligible for marriage`;
+    console.log(result);
+}femaleMarriageEligibillity("female",16,"Jenifer");
+femaleMarriageEligibillity("female",27,"Maliinda Gates");
