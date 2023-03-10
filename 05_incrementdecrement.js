078dcabb26fa6bdf230bc4b4f25ffac92de4790c
komalramdaskhalkar <komalramdaskhalkar@gmail.com>
2023-03-10 15:10:37 +0530

    06_for_assignment01.js completed
    06_for_assignment01.js completed

---
+++ 05_incrementdecrement.js
@@ -0,0 +1,6 @@
+
+
+
+var index=10;
+var y=index--
+console.log(`index:${index}, y:${y}`);
---
+++ 06_for_assignment01.js
@@ -0,0 +1,34 @@
+
+console.log("WAP to print numbers from 5 to 15 by incrementing 1");
+    for (let index = 5; index <=15; index=index+1) {
+        console.log(index);
+        
+    }
+    console.log("WAP to print numbers from 50 to 40 by decrementing 1");
+    for (let index = 50; index >=40; index=index-1) {
+        console.log(index);
+        
+    }
+    console.log("WAP find first 15 odd number");
+    for (let index =1; index<=15;index++) {
+        console.log(2*index-1);
+        
+    }
+    console.log("WAP find first 15 even number");
+    for (let index =1; index<=15;index++) {
+        console.log(2*index);
+        
+    }console.log("WAP print table of 5");
+    for (let index =5; index<=50;index=index+5) {
+        console.log(index);}
+
+        console.log("WAP print table 10");
+    for (let index =10; index<=100;index=index+10) {
+        console.log(index);}
+
+        console.log('Reveres number');
+for (let index =100;index >= 10;index=index-10) {
+    console.log(index);
+    }
+        
+    
